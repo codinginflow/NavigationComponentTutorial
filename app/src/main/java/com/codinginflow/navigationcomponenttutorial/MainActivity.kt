78948f466997e9512423215b5fefc7e3439f94cd
Florian Walther <flo.walther@gmx.de>
2020-10-05 12:00:00 +0200

    End of part 5

--- app/src/main/java/com/codinginflow/navigationcomponenttutorial/MainActivity.kt
+++ app/src/main/java/com/codinginflow/navigationcomponenttutorial/MainActivity.kt
@@ -2,10 +2,27 @@ package com.codinginflow.navigationcomponenttutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
