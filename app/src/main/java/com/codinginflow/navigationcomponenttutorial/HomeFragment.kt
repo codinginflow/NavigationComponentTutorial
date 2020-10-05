6020ef0d17695514f2e7c5b47b45d508c4142a74
Florian Walther <flo.walther@gmx.de>
2020-10-05 10:26:53 +0200

    End of part 3

--- app/src/main/java/com/codinginflow/navigationcomponenttutorial/HomeFragment.kt
+++ app/src/main/java/com/codinginflow/navigationcomponenttutorial/HomeFragment.kt
@@ -1,6 +1,19 @@
 package com.codinginflow.navigationcomponenttutorial
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button_login.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
+    }
 }
---
+++ app/src/main/java/com/codinginflow/navigationcomponenttutorial/LoginFragment.kt
@@ -0,0 +1,6 @@
+package com.codinginflow.navigationcomponenttutorial
+
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+}
