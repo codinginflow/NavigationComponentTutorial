10b1665881ad7957b0fd0c13b3ee5a596b1f8ade
Florian Walther <flo.walther@gmx.de>
2020-10-06 10:00:41 +0200

    End of part 9

--- app/src/main/java/com/codinginflow/navigationcomponenttutorial/LoginFragment.kt
+++ app/src/main/java/com/codinginflow/navigationcomponenttutorial/LoginFragment.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
+    private val args: LoginFragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val usernameDeepLink = args.username
+        edit_text_username.setText(usernameDeepLink)
 
         button_confirm.setOnClickListener {
             val username = edit_text_username.text.toString()
